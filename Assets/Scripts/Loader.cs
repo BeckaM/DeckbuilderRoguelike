a8e0316c0521ed1b2b274842c805a74d6ad04ef5
per.wahlstedt <per.wahlstedt@gmail.com>
2017-03-17 15:58:38 +0100

    Spinning sh*t!

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -7,10 +7,8 @@ namespace Assets.Scripts
 	public class Loader : MonoBehaviour 
 	{
 		public GameObject gameManager;			//GameManager prefab to instantiate.
-		public GameObject soundManager;         //SoundManager prefab to instantiate.
-      
-
-
+		public GameObject soundManager;         //SoundManager prefab to instantiate.    
+        
 
         void Awake ()
 		{
